1c788645383559dd580c599a547ede664660f0bc
Vedant Kavhar <vedant.22110832@viit.ac.in>
2025-06-11 13:22:56 +0530

    cors and env update to render/api

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:4200' }));
+// app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors()); // Allow all origins (less secure, only for dev)
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Normalized path for consistency
 
