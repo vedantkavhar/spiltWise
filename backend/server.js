dd05c961fa6b06ecf89c72ed15d730e7efc33177
Vedant Sanjay Kavhar <108593325+vedantkavhar@users.noreply.github.com>
2025-06-12 12:01:54 +0530

    Update server.js removed warning causing useNewUrlParser and useUnifiedTopology on deployment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,7 @@ app.use('/api/expenses', expenseRoutes);
 app.use('/api/categories', categoryRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
