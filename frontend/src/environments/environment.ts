c6af65b5224e8f657f81954436545de543073f55
vedantkavhar <vedant.22110832@viit.ac.in>
2025-06-26 14:45:22 +0530

    Add production environment configuration and file replacements for Angular build (env dev,env prod)

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:5000/api' // Ensured correct API base URL
-  apiUrl: 'https://spiltwise-backend.onrender.com/api' // Ensured correct API base URL
+  apiUrl: 'http://localhost:5000/api' // Ensured correct API base URL
 };
